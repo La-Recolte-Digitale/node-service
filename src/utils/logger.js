7518de9458508f36e27187a89377db2af8d44aa9
mathieu courbis <thierry.ferland@gmail.com>
2020-08-06 11:53:36 +0000

    lint

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,9 +1,9 @@
-const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, json } = format;
+const { createLogger, format, transports } = require('winston')
+const { combine, timestamp, json } = format
 
 const logger = createLogger({
   level: 'info',
-  format: combine( timestamp(), json()),
+  format: combine(timestamp(), json()),
   defaultMeta: { service: 'purchase-service' },
   transports: [new transports.Console()]
 })
