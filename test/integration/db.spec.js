5dab31ed1d34df52a5eb8a5301ea53a3de017fcf
tferland <thierry.ferland@gmail.com>
2020-10-15 11:19:30 +0200

    fix test

--- test/integration/db.spec.js
+++ test/integration/db.spec.js
@@ -1,4 +1,5 @@
-const logger = require('../../src/utils/logger')
+let db = require('../../src/utils/db')
+let logger = require('../../src/utils/logger')
 jest.mock('../../src/utils/logger')
 
 const timeout = (seconds) => {
@@ -11,19 +12,19 @@ const OLD_ENV = process.env
 
 beforeEach(async () => {
   jest.resetModules()
+  logger.info.mockClear()
   process.env = { ...OLD_ENV }
   delete process.env.DB_URI
 })
 
 afterEach(async () => {
-  await require('../../src/utils/db').closeConnection()
+  await db.closeConnection()
 })
 
 describe('Database', () => {
   describe('Connection', () => {
     it('is opened and closed', async () => {
-      process.env.DB_URI = 'mongodb://database:27017/recipe-db-test'
-      await require('../../src/utils/db').init()
+      await db.init()
       await timeout(1)
       expect(logger.info).toHaveBeenNthCalledWith(1, 'Mongoose default connection is open')
     })
@@ -32,11 +33,14 @@ describe('Database', () => {
       process.env.DB_URI = 'mongodb://database:27018/db'
       process.env.NODE_ENV = 'test'
       const config = require('../../config/config')
+      db = require('../../src/utils/db')
+      logger = require('../../src/utils/logger')
+      jest.mock('../../src/utils/logger')
       expect(config.database.uri).toBe('mongodb://database:27018/db')
-      await require('../../src/utils/db').init()
+      await db.init()
       await timeout(1)
-      expect(logger.info).toHaveBeenNthCalledWith(4, 'Cannot connect to database')
-      expect(logger.info).toHaveBeenNthCalledWith(5, 'An error on mongoose default connection has occurred')
+      expect(logger.info).toHaveBeenNthCalledWith(2, 'Cannot connect to database')
+      expect(logger.info).toHaveBeenNthCalledWith(3, 'An error on mongoose default connection has occurred')
     })
   })
 })
